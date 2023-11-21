be02545b8ad1c4a4302b9053946480a3e1c38c80
Jamie Sewell <jamiegsewell@gmail.com>
2023-11-21 00:44:48 +0000

    Add function to build a deck of cards

--- run.py
+++ run.py
@@ -2,3 +2,18 @@
 BlackJack
 """
 
+
+deck = []
+
+
+def build_deck():
+    global deck
+    suites = ['H', 'D', 'S', 'C']
+    for suite in suites:
+        for i in range(2, 11):
+            deck.append((suite, i))
+        deck.append((suite, 'J'))
+        deck.append((suite, 'Q'))
+        deck.append((suite, 'K'))
+        deck.append((suite, 'A'))
+    return deck
