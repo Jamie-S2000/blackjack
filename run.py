091b23a9695ee18d363edbf57b025eb074da5766
Jamie Sewell <jamiegsewell@gmail.com>
2023-11-21 01:00:53 +0000

    update suite and picture card names

--- run.py
+++ run.py
@@ -9,14 +9,14 @@ deck = []
 
 def build_deck():
     global deck
-    suites = ['H', 'D', 'S', 'C']
+    suites = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
     for suite in suites:
         for i in range(2, 11):
             deck.append((suite, i))
-        deck.append((suite, 'J'))
-        deck.append((suite, 'Q'))
-        deck.append((suite, 'K'))
-        deck.append((suite, 'A'))
+        deck.append((suite, 'Jack'))
+        deck.append((suite, 'Queen'))
+        deck.append((suite, 'King'))
+        deck.append((suite, 'Ace'))
     return deck
 
 
